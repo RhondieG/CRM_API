2021a5b17f16a2bf0320b22b39f8ef5cde53893a
Rhonda Gilliard <amourg08@gmail.com>
2020-05-11 20:06:03 -0400

    imports and routes

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from 'express';
+import routes from './src/routes/crmRoutes';
 
 const app = express();
-const PORT = 3030;
+const PORT = 5000;
+
+routes(app);
 
 app.get('/',(req, res) =>
      res.send(`Node and express server running on port ${PORT}`)
-     
      );
 
 app.listen(PORT, () =>
      console.log(`Your server is running on port ${PORT}`)
-     
      );
